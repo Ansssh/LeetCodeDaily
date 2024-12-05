7524aa21e01457b1f4a39ce5ab6154fbfbfee568
Anshh <anshkumar975310@gmail.com>
2024-12-05 22:12:54 +0530

    Spiral Matrix

--- Demitasse.java
+++ Demitasse.java
@@ -1,23 +1,57 @@
+import java.util.*;
+class Solution {
+    public static int[] spiralMatrix(int[][] mat) {
+        int n = 0, s = mat.length, e = 0, w = mat[0].length;
+        int[] res = new int[s * w];
+        int index = 0;
 
-public class Demitasse {
-    public static void main(String args[]) {
-        Solution x = new Solution();
-        int[] arr = {1,5};
-        System.out.println(x.addSpaces("ILoveCoding", arr));
+        while (n < s && e < w) {
+            
+            for (int i = e; i < w; i++) {
+                res[index] = mat[n][i];
+                index++;
+            }
+            n++; 
+
+            
+            for (int i = n; i < s; i++) {
+                res[index] = mat[i][w - 1];
+                index++;
+            }
+            w--; 
+
+            
+            if (n < s) { 
+                for (int i = w - 1; i >= e; i--) {
+                    res[index] = mat[s - 1][i];
+                    index++;
+                }
+                s--; 
+            }
+
+            
+            if (e < w) { 
+                for (int i = s - 1; i >= n; i--) {
+                    res[index] = mat[i][e];
+                    index++;
+                }
+                e++; 
+            }
+        }
+
+        return res;
     }
 }
 
-class Solution {
-    public String addSpaces(String s, int[] spaces) {
-        StringBuilder x = new StringBuilder();
-        int n = 0;
-
-        for(int i:spaces){
-            x.append(s.substring(n,i));
-            n = i;
-            x.append(" ");
-        }
-        x.append(s.substring(n));
-        return x.toString();
+public class Demitasse{
+    public static void main(String args[]){
+        int[][] matrix = 
+        {{1,2,3,4},
+        {1,2,3,4},
+        {1,2,3,4}};
+        int[] result = Solution.spiralMatrix(matrix);
+        int[] res = Arrays.copyOfRange(result, 0, matrix.length * matrix[0].length);
+        System.out.println(Arrays.toString(res));
+        
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ﻿# LeetCodeDaily
 
 03-12-2024 >>> Adding Spaces to a String
+05-12-2024 >>> Not from LC but still an important one! Spiral Matrix
