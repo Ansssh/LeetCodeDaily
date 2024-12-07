fc919d37cbe190227babdd2337e525bae0463a1d
Anshh <anshkumar975310@gmail.com>
2024-12-07 13:22:06 +0530

    Pascal Triangle

--- Demitasse.java
+++ Demitasse.java
@@ -1,64 +1,28 @@
 import java.util.ArrayList;
 
 class Solution {
-    ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m){
-        boolean[] rowsWithZero = new boolean[n];
-        boolean[] colsWithZero = new boolean[m];
-
-        for(int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++) {
-                if(matrix.get(i).get(j)==0){
-                    rowsWithZero[i] = true;
-                    colsWithZero[j] = true;
-                }
-            }
-        }
+    public ArrayList<ArrayList<Long>> printPascal(int n) {
+        ArrayList<ArrayList<Long>> sol = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if(colsWithZero[j] || rowsWithZero[i]){
-                    matrix.get(i).set(j, 0);
-                }
+            ArrayList<Long> init = new ArrayList<>();
+            init.add((long) 1);
+            for (int j = 1; j < i; j++) {
+                init.add(sol.get(i - 1).get(j - 1) + sol.get(i - 1).get(j));
             }
+            if (i > 0) {
+                init.add((long) 1);
+            }
+            sol.add(init);
         }
-        
-
-        return matrix;
-
+        return sol;
     }
-
 }
 
 public class Demitasse {
     public static void main(String args[]) {
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-        ArrayList<Integer> a1 = new ArrayList<>();
-        a1.add(1);
-        a1.add(2);
-        a1.add(3);
-        ArrayList<Integer> a2 = new ArrayList<>();
-        a2.add(0);
-        a2.add(1);
-        a2.add(7);
-        // a2.add(0);
-
-        arr.add(a1);
-        arr.add(a2);
-
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = 0; j < arr.get(i).size(); j++) {
-                System.out.print(arr.get(i).get(j) + " ");
-            }
-            System.out.println();
-        }
-
-        Solution x = new Solution();
-        ArrayList<ArrayList<Integer>> sol = x.zeroMatrix(arr, 2, 3);
-        ;
-        for (int i = 0; i < sol.size(); i++) {
-            for (int j = 0; j < sol.get(i).size(); j++) {
-                System.out.print(sol.get(i).get(j) + " ");
-            }
-            System.out.println();
-        }
+        ArrayList<ArrayList<Long>> x = new ArrayList<>();
+        Solution bankai = new Solution();
+        x = bankai.printPascal(6);
+        System.out.println(x);
     }
 }
