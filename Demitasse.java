37203ebbbe144cc4c69933235fc706dc1a2545ca
Anshh <anshkumar975310@gmail.com>
2024-12-19 17:35:35 +0530

    Lexicographically Smallest Array

--- Demitasse.java
+++ Demitasse.java
@@ -1,50 +1,182 @@
-class Solution {
-    public static int numberOfInversions(int[] arr, int n) {
-        return mergeSortAndCount(arr, 0, n - 1);
-    }
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
-    private static int mergeSortAndCount(int[] arr, int left, int right) {
-        int count = 0;
+// ? code by me
 
-        if (left < right) {
-            int mid = (left + right) / 2;
+class DSU{
+    private int[] nums;
+    private int[] indices;
 
-            count += mergeSortAndCount(arr, left, mid);
-
-            count += mergeSortAndCount(arr, mid + 1, right);
-
-            count += mergeAndCount(arr, left, mid, right);
+    DSU(int n){
+        nums = new int[n];
+        indices = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = i;
+            indices[i] = 0;
         }
-
-        return count;
     }
 
-    private static int mergeAndCount(int[] arr, int left, int mid, int right) {
-        int[] leftArr = java.util.Arrays.copyOfRange(arr, left, mid + 1);
-        int[] rightArr = java.util.Arrays.copyOfRange(arr, mid + 1, right + 1);
+    public int find(int x){
+        if(nums[x] != x){
+            nums[x] = find(nums[x]);
+        }
+        return nums[x];
+    }
 
-        int i = 0, j = 0, k = left, swaps = 0;
+    public void union(int index1, int index2){
+        int root1 = find(index1);
+        int root2 = find(index2);
 
-        while (i < leftArr.length && j < rightArr.length) {
-            if (leftArr[i] <= rightArr[j]) {
-                arr[k++] = leftArr[i++];
-            } else {
-                arr[k++] = rightArr[j++];
-                swaps += (leftArr.length - i);
+        if(root1 != root2){
+            if (indices[root1] > indices[root2]) {
+                nums[root2] = root1;
+            }else if(indices[root1] < indices[root2]){
+                nums[root1] = root2;
+            }else{
+                nums[root2] = root1;
+                indices[root1]++;
             }
         }
-        while (i < leftArr.length) arr[k++] = leftArr[i++];
-        while (j < rightArr.length) arr[k++] = rightArr[j++];
-
-        return swaps;
     }
 }
 
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n = nums.length;
+        DSU uf = new DSU(n);
 
-public class Demitasse {
-    public static void main(String args[]) {
-        int[] sol = {5,4,2,1,3};
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (Math.abs(nums[i] - nums[j] )<= limit) {
+                    uf.union(i, j);
+                }
+            }
+        }
+
+        HashMap<Integer,ArrayList<Integer>> adj = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int key = uf.find(i);
+            if(adj.containsKey(key)){
+                ArrayList<Integer> ol = adj.get(key);
+                ol.add(nums[i]);
+                adj.put(key, ol);
+            }else{
+                ArrayList<Integer> ol = new ArrayList<>();
+                ol.add(nums[i]);
+                adj.put(key, ol);
+            }
+        }
+
+        for (int i : adj.keySet()) {
+            adj.get(i).sort(null);
+        }
+
+        // System.out.println(adj);
+        // System.out.println(Arrays.toString(nums));
+
+        for (int i = 0; i < n; i++) {
+            int father = uf.find(i);
+            nums[i] = adj.get(father).get(0);
+            adj.get(father).remove(0);
+        }
+        
+        return nums;
+    }
+}
+
+public class Demitasse{
+    public static void main(String args[]){
+        int[] nums = {1,5,3,9,8};
         Solution x = new Solution();
-        System.out.println(x.numberOfInversions(sol, 5));
+        int[] sol = x.lexicographicallySmallestArray(nums, 3);
+        System.out.println(Arrays.toString(sol));
     }
 }
+
+// ? Code By GPT
+
+// import java.util.*;
+
+// class DSU {
+//     private int[] parent;
+//     private int[] rank;
+
+//     DSU(int n) {
+//         parent = new int[n];
+//         rank = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             parent[i] = i;
+//             rank[i] = 0;
+//         }
+//     }
+
+//     public int find(int x) {
+//         if (parent[x] != x) {
+//             parent[x] = find(parent[x]); // Path compression
+//         }
+//         return parent[x];
+//     }
+
+//     public void union(int x, int y) {
+//         int rootX = find(x);
+//         int rootY = find(y);
+
+//         if (rootX != rootY) {
+//             if (rank[rootX] > rank[rootY]) {
+//                 parent[rootY] = rootX;
+//             } else if (rank[rootX] < rank[rootY]) {
+//                 parent[rootX] = rootY;
+//             } else {
+//                 parent[rootY] = rootX;
+//                 rank[rootX]++;
+//             }
+//         }
+//     }
+// }
+
+// class Solution {
+//     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+//         int n = nums.length;
+//         DSU dsu = new DSU(n);
+
+//         // Pair the values with their indices and sort by value
+//         int[][] pairs = new int[n][2];
+//         for (int i = 0; i < n; i++) {
+//             pairs[i][0] = nums[i];
+//             pairs[i][1] = i;
+//         }
+//         Arrays.sort(pairs, Comparator.comparingInt(a -> a[0]));
+
+//         // Perform unions only between adjacent elements in the sorted array
+//         for (int i = 0; i < n - 1; i++) {
+//             if (Math.abs(pairs[i][0] - pairs[i + 1][0]) <= limit) {
+//                 dsu.union(pairs[i][1], pairs[i + 1][1]);
+//             }
+//         }
+
+//         // Group indices by their root and sort the values within each group
+//         Map<Integer, PriorityQueue<Integer>> groupedValues = new HashMap<>();
+//         for (int i = 0; i < n; i++) {
+//             int root = dsu.find(i);
+//             groupedValues.computeIfAbsent(root, k -> new PriorityQueue<>()).offer(nums[i]);
+//         }
+
+//         // Reconstruct the array by assigning the smallest value from each group
+//         for (int i = 0; i < n; i++) {
+//             int root = dsu.find(i);
+//             nums[i] = groupedValues.get(root).poll();
+//         }
+
+//         return nums;
+//     }
+// }
+
+// public class Demitasse {
+//     public static void main(String[] args) {
+//         int[] nums = {1, 5, 3, 9, 8};
+//         Solution solution = new Solution();
+//         int[] result = solution.lexicographicallySmallestArray(nums, 2);
+//         System.out.println("Lexicographically Smallest Array: " + Arrays.toString(result));
+//     }
+// }
--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 06-12-2024 >>> Not from LC but still an important one! Pascal Triangle
 06-12-2024 >>> Not from LC but still an important one! Inversions something?? (OH SHit! TLE)
 06-12-2024 >>> Not from LC but still an important one! Inversions something?? (Modified Merge Sort Recursive Approach)
+
+19-12-2024 >>> "Make Lexicographically Smallest Array by Swapping" (Oh Boy this shit is difficult, DIFFICULT, but still made it and optimized by GPT ofc)
